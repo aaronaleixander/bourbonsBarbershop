bdf65c8afdd5b4c4c819cfd33dd9ea2368112572
aau19 <aau19@DESKTOP-M0K5TMI>
2020-03-14 14:57:34 -0700

    Add data annotations for product class.

--- bourbonsBarbershop/Models/Products.cs
+++ bourbonsBarbershop/Models/Products.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,14 +8,35 @@ namespace bourbonsBarbershop.Models
 {
     public class Products
     {
+        [Key]
         public int ProductId { get; set; }
 
+        /// <summary>
+        /// The Product Type (gel, hairspray, pomade)
+        /// </summary>
+        [Required]
+        [Display(Name = "Product Type (i.e. gel, pomade, hairspray):")]
         public string ProductType { get; set; }
 
+        /// <summary>
+        /// The name of the product
+        /// </summary>
+        [Required]
+        [Display(Name = "Product Name:")]
         public string ProductName { get; set; }
 
+        /// <summary>
+        /// What does the product do?
+        /// </summary>
+        [Required]
+        [Display(Name = "Product Description:")]
         public string ProductDescription { get; set; }
 
+        /// <summary>
+        /// The price of the product
+        /// </summary>
+        [Required]
+        [Display(Name = "Price:")]
         public decimal ProductPrice { get; set; }
     }
 }
