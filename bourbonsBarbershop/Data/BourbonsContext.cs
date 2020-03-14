cf2c1fb263c8c0f8643b8374a13a7c26e9f1e2f2
aau19 <aau19@DESKTOP-M0K5TMI>
2020-03-13 17:22:30 -0700

    Finished Models

---
+++ bourbonsBarbershop/Data/BourbonsContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using bourbonsBarbershop.Models; 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bourbonsBarbershop.Data
+{
+    public class BourbonsContext : DbContext
+    {
+        public BourbonsContext(DbContextOptions<BourbonsContext> options) 
+            : base(options) { }
+        // Db Sets
+    }
+}
