e085ea385bcfeeb7beec4ee4b837e83e176920a8
aau19 <aau19@DESKTOP-M0K5TMI>
2020-03-15 11:08:34 -0700

    Changed hamepage image

---
+++ bourbonsBarbershop/Controllers/TalentController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bourbonsBarbershop.Controllers
+{
+    public class TalentController : Controller
+    {
+        public IActionResult Talent()
+        {
+            return View();
+        }
+    }
+}
